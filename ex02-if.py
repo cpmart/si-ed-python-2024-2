7dcdc008e899bd74014617885cdca357c4f3f2ac
cpmart <cpmart@gmail.com>
2024-08-30 20:02:22 -0300

    Básico

---
+++ ex02-if.py
@@ -0,0 +1,12 @@
+# Entre com a temperatura
+# Se for menor que 20, imprima frio
+# Se for maior igual a 20 e menor que 30, imprima agradável
+# Se for mario igual a 30, imprima quente 
+temperatura = float(input("Digite a temperatura"))
+
+if (temperatura < 20):
+    print("Frio")
+if(temperatura >= 20 and temperatura < 30):
+    print("Agradável")
+if(temperatura >= 30):
+    print("Quente")
